e01c3fd6aec491eb4e14014edde03149ea257d76
jang314 <jang314@naver.com>
2025-09-28 10:31:39 +0900

    first commit : TEMPLATE SERVICE 초기환경세팅

---
+++ template-serivce/src/main/java/com/ums/template/TemplateSerivceApplication.java
@@ -0,0 +1,13 @@
+package com.ums.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateSerivceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateSerivceApplication.class, args);
+    }
+
+}
